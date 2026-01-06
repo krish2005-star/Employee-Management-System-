68a57d68d1ed473f20decff89a11e864234638af
Kshitij <m.krishna0513@gmail.com>
2026-01-07 02:31:41 +0530

    updated gitignore file

---
+++ src/util/DatabaseConnection.java
@@ -0,0 +1,36 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnection {
+    private static final Properties props = new Properties();
+
+    static {
+        try {
+            FileInputStream fis = new FileInputStream("src/config.properties");
+            props.load(fis);
+        } catch (IOException e) {
+            System.out.println("Error loading config file: " + e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected successful");
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+            return null;
+        }
+    }
+}
